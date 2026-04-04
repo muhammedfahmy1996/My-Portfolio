efe3f938d3eaa4d7558204a96e9fccdd31db9567
Mohamed Fahmy <muhammedfahmy.business@gmail.com>
2026-04-04 08:19:53 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -17,8 +17,52 @@ $(function () {
     var target = $($(this).attr('href'));
     if (target.length) {
       $('html, body').animate({ scrollTop: target.offset().top - 70 }, 700, 'swing');
-      $('.navbar-collapse').collapse('hide');
     }
+  });
+
+  /* ---- Mobile Menu ---- */
+  var $toggler   = $('#menuToggler');
+  var $overlay   = $('#mobileOverlay');
+  var $closeBtn  = $('#menuClose');
+  var $mobileLinks = $('.mobile-nav-link');
+
+  function openMenu() {
+    $toggler.addClass('open');
+    $overlay.addClass('active');
+    $('body').css('overflow', 'hidden');
+  }
+
+  function closeMenu() {
+    $toggler.removeClass('open');
+    $overlay.removeClass('active');
+    $('body').css('overflow', '');
+  }
+
+  $toggler.on('click', openMenu);
+  $closeBtn.on('click', closeMenu);
+
+  // Close when clicking the backdrop (not the panel)
+  $overlay.on('click', function(e) {
+    if (!$(e.target).closest('#mobilePanel').length) {
+      closeMenu();
+    }
+  });
+
+  // Close on link click and scroll to section
+  $mobileLinks.on('click', function(e) {
+    e.preventDefault();
+    var target = $($(this).attr('href'));
+    closeMenu();
+    setTimeout(function() {
+      if (target.length) {
+        $('html, body').animate({ scrollTop: target.offset().top - 70 }, 700, 'swing');
+      }
+    }, 300);
+  });
+
+  // Close on ESC key
+  $(document).on('keydown', function(e) {
+    if (e.key === 'Escape') closeMenu();
   });
 
   /* ---- Active nav link ---- */
@@ -31,6 +75,8 @@ $(function () {
       if (scrollPos >= top && scrollPos < bottom) {
         $('.nav-link-custom').removeClass('active');
         $('.nav-link-custom[href="#' + id + '"]').addClass('active');
+        $('.mobile-nav-link').removeClass('active');
+        $('.mobile-nav-link[href="#' + id + '"]').addClass('active');
       }
     });
   }
